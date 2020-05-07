de3edfa1aad525631c4109bd0e8825c521d05f47
Vadim Zinoviev <vazinoviev2107@gmail.com>
2020-05-07 20:49:57 +0300

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,53 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    'use strict';
+
+    if (animal == undefined) {
+        return null;
+    }
+    
+    const sound = animal.sound;
+
+    if (animal !== undefined) {
+        return sound;
+    }    
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    'use strict';
+   
+    let sum = 0;
+    let average;
+    let roundedAverage;
+
+    if (marks.length == 0) {
+        return 0
+    } else {
+        for (let i = 0; i < marks.length; i++) {
+            sum += Number(marks[i]);
+        }
+
+        average = sum / marks.length;
+        roundedAverage = Math.round(average);
+    }    
+
+    return roundedAverage;    
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    'use strict';
+
+    birthday = +new Date(birthday);
+
+    let now = Date.now();
+    let diff = now - birthday;    
+    let age = Math.floor(diff / (365.25  * 24 * 3600 * 1000 ));
+    let verdict;
+
+    if (age >= 18) {
+        verdict = true;
+    } else {
+        verdict = false;
+    }
+
+    return verdict;
 }
